f460e46f20381de809f0894d4c31e19519f62898
Jose Luis Carrillo Aguilar <jose.carrillo@microsoft.com>
2023-06-09 04:59:43 +0200

    Admin enabled

--- src/container-registry/acr.tf
+++ src/container-registry/acr.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = "westeurope"
   sku                 = "Standard"
-  admin_enabled       = false
+  admin_enabled       = true
   identity {
     type = "UserAssigned"
     identity_ids = [
